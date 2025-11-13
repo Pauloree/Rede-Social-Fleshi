f1f5b51fd85389a940ca973bd30af77e345f6a80
Paulo <paulo2renato88@gmail.com>
2025-11-13 16:55:00 -0300

    feat: database configuration and modeling

--- appfleshi/models.py
+++ appfleshi/models.py
@@ -1,7 +1,16 @@
 from appfleshi import database
+from zoneinfo import ZoneInfo
+from datetime import datetime
 
 class User(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20), unique=True, nullable=False)
+    email = database.Column(database.String(100), unique=True, nullable=False)
+    password = database.Column(database.String(60), nullable=False)
+    photos = database.relationship('Photo', backref='user', lazy=True)
 
 class Photo(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+    file_name = database.Column(database.String(255), nullable=False, default='default.png')
+    upload_date = database.Column(database.DateTime, nullable=False, default=lambda:datetime.now(ZoneInfo('America/Sao_Paulo')))
