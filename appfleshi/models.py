06e21ea8fbaf2d93ad07297ddd81c56580b2aad8
Aluno <paulo2renato88@gmail.com>
2025-11-19 14:47:00 -0300

    feat: login and security system setup

--- appfleshi/models.py
+++ appfleshi/models.py
@@ -1,15 +1,20 @@
-from appfleshi import database
+from appfleshi import database, login_manager
 from zoneinfo import ZoneInfo
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(database.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(20), unique=True, nullable=False)
     email = database.Column(database.String(100), unique=True, nullable=False)
     password = database.Column(database.String(60), nullable=False)
     photos = database.relationship('Photo', backref='user', lazy=True)
 
-class Photo(database.Model):
+class Photo(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
     file_name = database.Column(database.String(255), nullable=False, default='default.png')
